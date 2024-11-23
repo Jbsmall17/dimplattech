ed997df4b21aec1a145196df14cb806f416f3a64
Ajibola <alaomiyaki17@gmail.com>
2024-11-23 23:27:28 +0100

    fixed deployment error

--- dimplattech/src/components/Sidebar.tsx
+++ dimplattech/src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { FaUserCircle } from "react-icons/fa";
 import { MdPayment } from "react-icons/md";
 import { IoNotifications, IoLogOut } from "react-icons/io5";

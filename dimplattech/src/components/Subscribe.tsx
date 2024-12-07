c7bcd97251890492cb08e007f6a7e68555c5da95
Ajibola <alaomiyaki17@gmail.com>
2024-12-07 12:21:51 +0100

    integrated mailchimp into the website

--- dimplattech/src/components/Subscribe.tsx
+++ dimplattech/src/components/Subscribe.tsx
@@ -1,8 +1,20 @@
 "use client"
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import { motion } from "motion/react"
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import Spinner from './Spinner';
+import toast, {Toaster} from 'react-hot-toast';
+
+interface CustomFormProps {
+  status: 'sending' | 'error' | 'success' | null; 
+  message: string | Error | null;              
+  onValidated: (formData: { EMAIL: string }) => void; 
+}
+
+
 
 export default function Subscribe() {
+  console.log(process.env.NEXT_PUBLIC_MAILCHIMP_ENDPOINT)
   return (
     <motion.section 
       initial={{opacity:0, y: 50}} 
@@ -14,12 +26,67 @@ export default function Subscribe() {
         <p className='text-xl text-white'>Subscribe to Our News Letter</p>
         <p className='text-sm text-white'>Subscribe today and get special offers, coupons and news</p>
       </div>
-      <div className='flex-1'>
-        <div className='w-full relative'>
-            <input className='input-el h-full p-3 w-full rounded-full' type="text" placeholder='Enter your email address' />
-            <button className='absolute z-10 top-[50%] right-3 leading-none p-2 -translate-y-[50%] rounded-full bg-black text-white'>Subscribe</button>
-        </div>
-      </div>
+      <MailchimpSubscribe
+        url={process.env.NEXT_PUBLIC_MAILCHIMP_ENDPOINT || ''}
+        render={({ subscribe, status, message }) => (
+          <CustomForm
+            status={status}
+            message={message}
+            onValidated={(formData: any) => subscribe(formData)}
+          />
+        )}
+      />
+      {/* <CustomForm /> */}
     </motion.section>
+  )
+}
+
+
+function CustomForm({status,message,onValidated}:CustomFormProps){
+  const [email,setEmail] = useState("")
+  const handleSubmit = (e: FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    if (!email.trim()){
+      toast.error("please provide a an email address")
+      return
+    }
+    if(!email.includes('@')){
+      toast.error("Please enter a valid email address")
+      return 
+    }
+
+    onValidated({EMAIL : email})
+
+    switch(message){
+      case "You're already subscribed, your profile has been updated. Thank you!":
+        toast.error("Email already exists!");
+        break;
+      case "Thank you for subscribing!":
+        toast.success("Thank you for subscribing!");
+        break;
+      default:
+          toast.error("An error ocurred. Please try again")
+    }
+    setEmail('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='flex-1'>
+      <Toaster />
+    <div className='w-full relative'>
+        <input 
+          className='input-el h-full p-3 w-full rounded-full' 
+          type="text" 
+          placeholder='Enter your email address'
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)} 
+        />
+        <button 
+          className='absolute z-10 top-[50%] right-3 leading-none p-2 -translate-y-[50%] w-[84px] h-[32px] rounded-full bg-black text-white hover:bg-white hover:text-black hover:border hover:border-black duration-500 transition'
+        >
+          {status == "sending" ? <Spinner /> : 'Subscribe'}
+        </button>
+    </div>
+  </form>
   )
 }
